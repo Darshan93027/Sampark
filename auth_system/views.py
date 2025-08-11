82d9d43d87a3fec8465a2c0d46b070625be1566a
Darshan Sharma <contactdarshan07@gmail.com>
2025-08-11 13:41:22 +0530

    Update views.py

--- auth_system/views.py
+++ auth_system/views.py
@@ -10,11 +10,6 @@ from django.contrib.auth import authenticate
 from mail.models import APIKey, OTPConfiguration
 import uuid
 
-from rest_framework.permissions import AllowAny
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework_simplejwt.tokens import RefreshToken as JWTRefreshToken
 class Signup(APIView):
     permission_classes = [AllowAny]
 
